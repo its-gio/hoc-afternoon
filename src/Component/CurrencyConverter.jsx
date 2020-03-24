ed67bd3d0c3d1d0c69ce06335c6252abc7d043c9
Giovani Aguirre <me@its-g.io>
2020-03-23 17:04:56 -0700

    added BaseComponent, select loop for currencyData, and buttons (+, -) to converter component.

--- src/Component/CurrencyConverter.jsx
+++ src/Component/CurrencyConverter.jsx
@@ -1,10 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+const currencyData = [
+	{ name: 'Japanese Yen', symbol: '¥', rate: 113.6, id: 0 },
+	{ name: 'British Pound', symbol: '£', rate: 0.77, id: 1 },
+	{ name: 'Canadian Dollar', symbol: 'CAD', rate: 1.32, id: 2 },
+	{ name: 'Mexican Peso', symbol: 'Mex$', rate: 20.41, id: 3 },
+	{ name: 'Swiss Franc', symbol: 'Fr.', rate: 1.01, id: 4 }
+]
 
 const withCurrency = (BaseComponent) => (
   class Currency extends Component {
+    state = {
+      currencyChosen : false,
+      selectedCurrency: 'Select Currency',
+      amount: 0
+    }
+
     render() {
+      const currencyList = currencyData.map(currency => <option key={currency.id} value={i}>{currency.name}</option>)
       return (
-        <h1>Hello!</h1>
+        <>
+          <select value={this.state.selectedCurrency}>
+            <option disabled value='Select Currency'>Select Currency</option>
+            {currencyList}
+          </select>
+
+          <button>+</button>
+          <button>-</button>
+
+          <BaseComponent
+						currency={currencyData[this.state.selectedCurrency]}
+						amount={this.state.amount}
+					/>
+        </>
       )
     }
   }
